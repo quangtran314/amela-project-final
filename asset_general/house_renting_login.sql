bd1d4bd05d2b8bf1371de7b899b3704e4edd9bc5
CuongTrinh1280 <58588543+CuongTrinh1280@users.noreply.github.com>
2021-08-14 10:53:54 +0700

    Sửa conflict

--- asset_general/house_renting_login.sql
+++ asset_general/house_renting_login.sql
@@ -1,9 +1,8 @@
-INSERT INTO house_renting.login (id, username, password) VALUES (1, 'admin', 'admin');
-
-
 create table login
 (
     id int auto_increment primary key,
     username varchar(50) charset utf8 null,
     password varchar(50) charset utf8 null
 );
+
+INSERT INTO house_renting.login (id, username, password) VALUES (1, 'admin', 'admin');
