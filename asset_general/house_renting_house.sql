479521d48d8a6ff2418a66a234fdd4c0bfb7b886
CuongTrinh1280 <58588543+CuongTrinh1280@users.noreply.github.com>
2021-08-16 15:21:39 +0700

    Cập nhật house_renting_house.sql #2

--- asset_general/house_renting_house.sql
+++ asset_general/house_renting_house.sql
@@ -14,8 +14,9 @@ create table house
 )
     charset = utf8mb4;
 
-INSERT INTO house_renting.house (house_id, house_name, type_id, price, address, numBathrooms, numBedrooms, des) VALUES (1, 'house1', 1, 1000, 'Hà Nam', 1, 1, null);
-INSERT INTO house_renting.house (house_id, house_name, type_id, price, address, numBathrooms, numBedrooms, des) VALUES (2, 'house2', 2, 1000, 'Hà Đông', 2, 2, null);
-INSERT INTO house_renting.house (house_id, house_name, type_id, price, address, numBathrooms, numBedrooms, des) VALUES (3, 'house3', 3, 1000, 'Hà Tây', 3, 3, null);
-INSERT INTO house_renting.house (house_id, house_name, type_id, price, address, numBathrooms, numBedrooms, des) VALUES (4, 'house4', 4, 1000, 'Hà Hồ', 4, 4, null);
-INSERT INTO house_renting.house (house_id, house_name, type_id, price, address, numBathrooms, numBedrooms, des) VALUES (5, 'house5', 5, 1000, 'Hà Giang', 5, 5, null);
+INSERT INTO house_renting.house (house_id, house_name, type_id, price, address, numBathrooms, numBedrooms, des) VALUES (1, 'house1', 1, 1000, 'Hà Nam', 1, 1, 'Mô tả 1');
+INSERT INTO house_renting.house (house_id, house_name, type_id, price, address, numBathrooms, numBedrooms, des) VALUES (2, 'house2', 2, 1000, 'Hà Đông', 2, 2, 'Mô tả 2');
+INSERT INTO house_renting.house (house_id, house_name, type_id, price, address, numBathrooms, numBedrooms, des) VALUES (3, 'house3', 3, 1000, 'Hà Tây', 3, 3, 'Mô tả 3');
+INSERT INTO house_renting.house (house_id, house_name, type_id, price, address, numBathrooms, numBedrooms, des) VALUES (4, 'house4', 4, 1000, 'Hà Hồ', 4, 4, 'Mô tả 4');
+INSERT INTO house_renting.house (house_id, house_name, type_id, price, address, numBathrooms, numBedrooms, des) VALUES (5, 'house5', 5, 1000, 'Hà Giang', 5, 5, 'Mô tả 5
+');
