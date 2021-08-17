eb515506ceb8fcbbcc0fbfeda85f08db6a320b9c
Quang Tran <quangshinoda1511@gmail.com>
2021-08-17 10:02:22 +0700

    Add house create feature and modify some field in House class

--- src/main/java/com/amela/controller/HouseController.java
+++ src/main/java/com/amela/controller/HouseController.java
@@ -2,15 +2,17 @@ package com.amela.controller;
 
 
 import com.amela.model.house.House;
+import com.amela.model.house.Type;
 import com.amela.service.house.IHouseService;
+import com.amela.service.house.IHouseTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Optional;
@@ -21,6 +23,14 @@ public class HouseController {
     @Autowired
     private IHouseService houseService;
 
+    @Autowired
+    private IHouseTypeService houseTypeService;
+
+    @ModelAttribute("houseTypes")
+    public Iterable<Type> houseTypes(){
+        Iterable<Type> types = houseTypeService.findAll();
+        return types;
+    }
 
     @GetMapping("/houses")
     public ModelAndView listHouses(){
@@ -39,4 +49,23 @@ public class HouseController {
         return modelAndView;
     }
 
+    @GetMapping("/create-house")
+    public ModelAndView showCreateHouse(){
+        ModelAndView modelAndView = new ModelAndView("/house/create");
+        modelAndView.addObject("house", new House());
+        Iterable<Type> temp = houseTypes();
+        return modelAndView;
+    }
+
+    @PostMapping("/create-house")
+    public ModelAndView saveHouse(@Validated @ModelAttribute("house") House house, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            ModelAndView modelAndView = new ModelAndView("/house/create");
+            return modelAndView;
+        }
+        houseService.save(house);
+        ModelAndView modelAndView = new ModelAndView("redirect:/houses");
+        modelAndView.addObject("message", "New note created successfully");
+        return modelAndView;
+    }
 }
