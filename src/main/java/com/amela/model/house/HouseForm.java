8a7c596cd60c42ff0850a3bdb572a3188c35a927
Manh Toan <nmt15300@gmail.com>
2021-08-19 17:21:01 +0700

    complete upload image

--- src/main/java/com/amela/model/house/HouseForm.java
+++ src/main/java/com/amela/model/house/HouseForm.java
@@ -10,6 +10,9 @@ import java.util.List;
 
 public class HouseForm {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long house_id;
 
     @NotNull
     @Size(min = 5, max = 20)
@@ -57,16 +60,17 @@ public class HouseForm {
         this.sourcePath = sourcePath;
     }
 
-//    public HouseForm(String house_name, String address, int numBedrooms, int numBathrooms, String des, float price, Type type , MultipartFile sourcePath) {
-//        this.house_name = house_name;
-//        this.address = address;
-//        this.numBedrooms = numBedrooms;
-//        this.numBathrooms = numBathrooms;
-//        this.des = des;
-//        this.price = price;
-//        this.type = type;
-//        this.sourcePath = sourcePath;
-//    }
+    public HouseForm(long house_id, String house_name, String address, int numBedrooms, int numBathrooms, String des, float price, Type type , MultipartFile sourcePath) {
+        this.house_id = house_id;
+        this.house_name = house_name;
+        this.address = address;
+        this.numBedrooms = numBedrooms;
+        this.numBathrooms = numBathrooms;
+        this.des = des;
+        this.price = price;
+        this.type = type;
+        this.sourcePath = sourcePath;
+    }
 
     public MultipartFile getSourcePath() {
         return sourcePath;
@@ -76,13 +80,13 @@ public class HouseForm {
         this.sourcePath = sourcePath;
     }
 
-//    public long getHouse_id() {
-//        return house_id;
-//    }
-//
-//    public void setHouse_id(long house_id) {
-//        this.house_id = house_id;
-//    }
+    public long getHouse_id() {
+        return house_id;
+    }
+
+    public void setHouse_id(long house_id) {
+        this.house_id = house_id;
+    }
 
     public String getHouse_name() {
         return house_name;
